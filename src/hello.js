5e98c649eb439b04b401d40ed5089643b25b7ffc
gotou <whitelate@gmail.com>
2018-11-21 11:56:35 +0900

    bug:動かない、謎。

---
+++ src/hello.js
@@ -0,0 +1,18 @@
+export default class Hello {
+	//コンストラクタ
+	constructor() {
+
+	}
+
+	/**
+	 * 
+	 * @returns {string}
+	 */
+
+	sayHello() {
+		const hello = 'Hi,there!';
+		console.log(hello);
+		return hello;
+	}
+
+}
