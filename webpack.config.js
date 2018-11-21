276b49fdf5825b64bc1c972fbb3276f4a36ecb70
gotou <whitelate@gmail.com>
2018-11-21 12:36:32 +0900

    babel

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,8 @@
 // Node.jsの標準モジュール　ファイルやパス関連のユーティリティ
 const path = require('path');
 
+
+
 module.exports = {
     // webpack4以降は設定しないと警告が出る。最適化するかどうか。今はしない。（開発なので）
     mode: 'development',
@@ -27,5 +29,30 @@ module.exports = {
         // そしてumdはライブラリ化するときの方法
         libraryTarget: 'umd'
         
-    }
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /(node_modules|bower_components)/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: [
+                            [
+                                "@babel/preset-env",
+                                {
+                                    "useBuiltIns": "usage",
+                                    // 市場シェアが0.25％を超えるブラウザー実行可能な最低限のコード出力
+                                    "targets": "> 0.25%, not dead"
+                                }
+                            ]
+                        ]
+                    }
+                }
+            }
+        ]
+    },
+    // ソースマップの出力を有効
+    devtool: 'inline-source-map'
 };
